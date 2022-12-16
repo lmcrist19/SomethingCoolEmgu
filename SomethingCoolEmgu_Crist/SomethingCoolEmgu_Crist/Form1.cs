8013d6f6ac793545666179062977bae03b9ee13d
lmcrist19 <98054225+lmcrist19@users.noreply.github.com>
2022-12-15 21:31:22 -0500

    Add files via upload

---
+++ SomethingCoolEmgu_Crist/SomethingCoolEmgu_Crist/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace SomethingCoolEmgu_Crist
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                capture = new VideoCapture(ofd.FileName);
+                Mat m = new Mat();
+                capture.Read(m);
+                pictureBox1.Image = m.ToBitmap();
+            }
+        }
+        private async void detectTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (capture == null)
+            {
+                return;
+            }
+
+            try
+            {
+                while (true)
+                {
+                    Mat m = new Mat();
+                    capture.Read(m);
+
+                    if (!m.IsEmpty)
+                    {
+                        pictureBox2.Image = m.ToBitmap();
+                        DetectText(m.ToImage<Bgr, byte>());
+                        double fps = capture.Get(Emgu.CV.CvEnum.CapProp.Fps);
+                        await Task.Delay(1000 / Convert.ToInt32(fps));
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void DetectText(Image<Bgr, byte> img)
+        {
+            Image<Gray, byte> sobel = img.Convert<Gray, byte>().Sobel(1, 0, 3).AbsDiff(new Gray(0.0)).Convert<Gray, byte>().ThresholdBinary(new Gray(30), new Gray(200));
+            Mat SE = CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Rectangle, new Size(10, 2), new Point(-1, -1));
+            sobel = sobel.MorphologyEx(Emgu.CV.CvEnum.MorphOp.Dilate, SE, new Point(-1, -1), 1, Emgu.CV.CvEnum.BorderType.Reflect, new MCvScalar(255));
+            Emgu.CV.Util.VectorOfVectorOfPoint contours = new Emgu.CV.Util.VectorOfVectorOfPoint();
+            Mat m = new Mat();
+
+            CvInvoke.FindContours(sobel, contours, m, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+
+            List<Rectangle> list = new List<Rectangle>();
+
+            for (int i = 0; i < contours.Size; i++)
+            {
+                Rectangle brect = CvInvoke.BoundingRectangle(contours[i]);
+
+                double ar = brect.Width / brect.Height;
+                if (ar > 2 && brect.Width > 15 && brect.Height > 8 && brect.Height < 100)
+                {
+                    list.Add(brect);
+                }
+            }
+            Image<Bgr, byte> imgout = img.CopyBlank();
+            foreach (var r in list)
+            {
+                CvInvoke.Rectangle(img, r, new MCvScalar(0, 0, 255), 2);
+                CvInvoke.Rectangle(imgout, r, new MCvScalar(0, 255, 255), -1);
+            }
+            imgout._And(img);
+            pictureBox1.Image = img.ToBitmap();
+            pictureBox2.Image = imgout.ToBitmap();
+        }
+
+
+    }
+}
+
